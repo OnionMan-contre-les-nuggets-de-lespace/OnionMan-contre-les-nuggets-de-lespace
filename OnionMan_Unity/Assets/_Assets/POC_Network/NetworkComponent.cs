1c0cadab9f7090bf4a72b03a6a6f5cfecd53c9aa
PaulLuneau <paul.luneau@mail.com>
2022-12-14 19:37:49 +0100

    Network first scripts (Unity side)

---
+++ OnionMan_Unity/Assets/_Assets/POC_Network/NetworkComponent.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using System.Threading.Tasks;
+
+public class NetworkComponent : MonoBehaviour
+{
+    //private UdpClient m_udpRecieveClient;
+    [SerializeField]
+    private UdpClient m_udpSendClient;
+    [SerializeField]
+    private int m_recievePort = 3001;
+    [SerializeField]
+    private int m_sendPort = 3002;
+
+    [SerializeField]
+    private string m_sendIP = "127.0.0.1";
+
+    [SerializeField]
+    string m_sentMessage = string.Empty;
+
+
+    bool wasSending = false;
+
+    void Start()
+    {
+        //m_udpRecieveClient = new UdpClient();
+        //m_udpRecieveClient.Client.Bind(new IPEndPoint(IPAddress.Parse(m_sendIP), m_recievePort));
+        m_udpSendClient = new UdpClient();
+        m_udpSendClient.Client.Bind(new IPEndPoint(IPAddress.Any, m_sendPort));
+
+        //StartCoroutine(RecieveCoroutine());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            if (!wasSending)
+            {
+                var data = Encoding.UTF8.GetBytes(m_sentMessage);
+                m_udpSendClient.Send(data, data.Length, m_sendIP, m_sendPort);
+                Debug.LogWarning("Sent Message");
+                wasSending = true;
+            }
+        }
+        else
+        {
+            wasSending = false;
+        }
+    }
+
+    private IEnumerator RecieveCoroutine()
+    {
+        var from = new IPEndPoint(IPAddress.Any, m_recievePort);
+        while (Application.isPlaying)
+        {
+            //var recvBuffer = m_udpRecieveClient.Receive(ref from);
+            //Debug.LogError(Encoding.UTF8.GetString(recvBuffer));
+            yield return null;
+        }
+    }
+}
