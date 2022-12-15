269929275dd1e77ea90bc4b6d13cdc3daed5c66b
PaulLuneau <paul.luneau@mail.com>
2022-12-15 10:59:53 +0100

    Deleted Unreal project

--- OnionMan_Unreal/Source/OnionMan_Unreal/OnionMan_Unreal.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "OnionMan_Unreal.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, OnionMan_Unreal, "OnionMan_Unreal" );
