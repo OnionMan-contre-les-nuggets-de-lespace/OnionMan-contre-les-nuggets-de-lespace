7c23ec91067f48ba2e61e28640fb9c73e93c5a58
paulluneaug <paul.luneau@mail.com>
2022-12-18 16:02:56 +0100

    Updated Tests

--- OnionMan_Unity/Assets/_Assets/Network/Tests/NetworkTests.cs
+++ OnionMan_Unity/Assets/_Assets/Network/Tests/NetworkTests.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using OnionMan.Network;
+using System.Linq;
 
-public class NetworkTests : MonoBehaviour
+public class NetworkTests : SynchronizedMonoBehaviour
 {
     [SerializeField]
-    private SynchronizedProperty<char> st;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private SynchronizedProperty<char> SP0;
+    [SerializeField]
+    private SynchronizedProperty<uint> SP1;
+    [SerializeField]
+    private SynchronizedProperty<string> SP2;
+    [SerializeField]
+    private SynchronizedProperty<long> SP3;
+
 
     // Update is called once per frame
     void Update()
@@ -22,9 +25,25 @@ public class NetworkTests : MonoBehaviour
 
     public void EncodeString()
     {
-        byte[] encoded = st.Encode();
-        var decoded = EncodingUtility.Decode<char>(encoded, 0);
-        Debug.LogError(EncodingUtility.PrintBytes(encoded));
-        Debug.LogError(decoded);
+        byte[] encoded = GetEncodedProperties();
+        int offset = 0;
+        var decodedSP0 = SP0.Decode(encoded, ref offset);
+        var decodedSP1 = SP1.Decode(encoded, ref offset);
+        var decodedSP2 = SP2.Decode(encoded, ref offset);
+        var decodedSP3 = SP3.Decode(encoded, ref offset);
+        Debug.LogError(EncodingUtility.GetBytesAsString(encoded));
+        Debug.LogError(decodedSP0);
+        Debug.LogError(decodedSP1);
+        Debug.LogError(decodedSP2);
+        Debug.LogError(decodedSP3);
+    }
+
+    public override byte[] GetEncodedProperties()
+    {
+        return SP0.Encode()
+            .Concat(SP1.Encode())
+            .Concat(SP2.Encode())
+            .Concat(SP3.Encode())
+            .ToArray();
     }
 }
