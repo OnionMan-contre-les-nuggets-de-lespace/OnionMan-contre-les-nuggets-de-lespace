194e08dd96be0efa74814f1ce595047239cc1a85
paulluneaug <paul.luneau@mail.com>
2022-12-22 19:42:46 +0100

    Forgotten ISingleton interface

--- OnionMan_Unity/Assets/_Assets/Utils/Singleton/ISingleton.cs
+++ OnionMan_Unity/Assets/_Assets/Utils/Singleton/ISingleton.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface ISingleton<T> where T : ISingleton<T>
+public interface ISingleton<T>/* where T : ISingleton<T>*/
 {
     public static T Instance { get; }
 }
