d3635b3dc1cf04e50f9ec69f70efad5d3d1c1a71
paulluneaug <paul.luneau@mail.com>
2022-12-22 19:46:53 +0100

    As SyncObj now store their properties, they can be responsible for encoding and decoding them

--- OnionMan_Unity/Assets/_Assets/Network/SynchronizedMonoBehaviour.cs
+++ OnionMan_Unity/Assets/_Assets/Network/SynchronizedMonoBehaviour.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using UnityEngine;
 
 namespace OnionMan.Network
@@ -8,25 +10,91 @@ namespace OnionMan.Network
     {
         #region ISynchronizedObject Implementation
         public uint ObjectID { get => m_ObjectID; }
-        public bool NeedSynchronization 
+        public bool NeedSync 
         { 
-            get => m_needSynchronization; 
-            set => m_needSynchronization = value; 
+            get => AnyPropertyNeedSync(); 
+            set => ResetNeedSync(value); 
+        }
+        public Dictionary<ushort, ISynchronizedProperty> Properties 
+        { 
+            get => m_synchronizedProperties;
         }
 
         [SerializeField]
         private uint m_ObjectID;
-        [SerializeField]
-        private bool m_needSynchronization;
 
-        public abstract IEnumerable<byte> GetEncodedProperties();
+        private Dictionary<ushort, ISynchronizedProperty> m_synchronizedProperties = new Dictionary<ushort, ISynchronizedProperty>();
 
-        public abstract void DecodeProperties(byte[] encodedProperties, ref int offset);
+        public IEnumerable<byte> EncodeObject(bool forSync = true)
+        {
+            IEnumerable<byte> result = new byte[0];
+            foreach (ISynchronizedProperty property in GetPropertiesToSync())
+            {
+                result = result.Concat(property.EncodeProperty(forSync));
+            }
+            return result;
+        }
+
+        public void DecodeObject(byte[] encodedProperties, ref int offset, int size)
+        {
+            while (offset < size)
+            {
+                int propertySize = EncodingUtility.Decode<int>(encodedProperties, ref offset);
+                int initialOffset = offset;
+
+                ushort propertyID = EncodingUtility.Decode<ushort>(encodedProperties, ref offset);
+                int dataSize = propertySize - sizeof(ushort);
+
+                if (m_synchronizedProperties.TryGetValue(propertyID, out ISynchronizedProperty synchronizedProperty))
+                {
+                    synchronizedProperty.DecodeProperty(encodedProperties, ref offset, dataSize);
+                    if (offset - initialOffset != propertySize)
+                    {
+                        Debug.LogError("Offset Overflow !");
+                    }
+                }
+                else
+                {
+                    throw new InvalidDataException($"There are no object with ID {propertyID}");
+                }
+            }
+        }
+
+        public abstract void LoadProperties();
+
+        private bool AnyPropertyNeedSync()
+        {
+            return GetPropertiesToSync().Any();
+        }
+
+        private void ResetNeedSync(bool state)
+        {
+            foreach(ISynchronizedProperty prop in m_synchronizedProperties.Values) 
+            {
+                prop.NeedSync = state;
+            }
+        }
+
+        public IEnumerable<ISynchronizedProperty> GetPropertiesToSync()
+        {
+            return m_synchronizedProperties.Values.Where((ISynchronizedProperty prop) => prop.NeedSync);
+        }
+
+        public void AddSynchronizedProperty(ISynchronizedProperty synchronizedProperty)
+        {
+            ushort propID = synchronizedProperty.PropertyID;
+            if (m_synchronizedProperties.ContainsKey(propID))
+            {
+                throw new InvalidDataException($"The ID {propID} is already used by {m_synchronizedProperties[propID]}");
+            }
+            m_synchronizedProperties.Add(propID, synchronizedProperty);
+        }
         #endregion
 
         // Start is called before the first frame update
         protected virtual void Start()
         {
+            LoadProperties();
             NetworkManager.Instance.AddSynchronizedObject(this);
         }
     }
