5d4c16373fffe13c95f572f4a4f2cbaa6ecd7cd1
paulluneaug <paul.luneau@mail.com>
2023-01-20 20:43:26 +0100

    Complete EncodingUtility

--- OnionMan_Unreal/Source/OnionMan_Unreal/Private/EncodingUtility.cpp
+++ OnionMan_Unreal/Source/OnionMan_Unreal/Private/EncodingUtility.cpp
@@ -6,12 +6,126 @@
 
 namespace OnionMan::Network 
 {
+#pragma region PutEncodedValueInBuffer
+
+    template<typename T>
+    void EncodingUtility::PutEncodedValueInBuffer(T value, TArray<uint8>& buffer, int& offset)
+    {
+    }
+
+    template<>
+    void EncodingUtility::PutEncodedValueInBuffer<FString>(FString value, TArray<uint8>&buffer, int& offset)
+    {
+        PutToBuffer(buffer, (const uint8*)TCHAR_TO_UTF8(*value), offset, value.Len());
+    }
+
+    template<>
+    void EncodingUtility::PutEncodedValueInBuffer<bool>(bool value, TArray<uint8>& buffer, int& offset)
+    {
+        PutToBuffer(buffer, (uint8*)&value, offset, sizeof(bool));
+    }
+
+    template<>
+    void EncodingUtility::PutEncodedValueInBuffer<char>(char value, TArray<uint8>& buffer, int& offset)
+    {
+        PutToBuffer(buffer, (uint8*)&value, offset, sizeof(char));
+    }
+
+    template<>
+    void EncodingUtility::PutEncodedValueInBuffer<double>(double value, TArray<uint8>& buffer, int& offset)
+    {
+        PutToBuffer(buffer, (uint8*)&value, offset, sizeof(double));
+    }
+
+    template<>
+    void EncodingUtility::PutEncodedValueInBuffer<float>(float value, TArray<uint8>& buffer, int& offset)
+    {
+        PutToBuffer(buffer, (uint8*)&value, offset, sizeof(float));
+    }
+
+    template<>
+    void EncodingUtility::PutEncodedValueInBuffer<int>(int value, TArray<uint8>& buffer, int& offset)
+    {
+        PutToBuffer(buffer, (uint8*)&value, offset, sizeof(int));
+    }
+
+    template<>
+    void EncodingUtility::PutEncodedValueInBuffer<long>(long value, TArray<uint8>& buffer, int& offset)
+    {
+        PutToBuffer(buffer, (uint8*)&value, offset, sizeof(long));
+    }
+
+    template<>
+    void EncodingUtility::PutEncodedValueInBuffer<short>(short value, TArray<uint8>& buffer, int& offset)
+    {
+        PutToBuffer(buffer, (uint8*)&value, offset, sizeof(short));
+    }
+
+    template<>
+    void EncodingUtility::PutEncodedValueInBuffer<uint32>(uint32 value, TArray<uint8>& buffer, int& offset)
+    {
+        PutToBuffer(buffer, (uint8*)&value, offset, sizeof(uint32));
+    }
+
+    template<>
+    void EncodingUtility::PutEncodedValueInBuffer<uint64>(uint64 value, TArray<uint8>& buffer, int& offset)
+    {
+        PutToBuffer(buffer, (uint8*)&value, offset, sizeof(uint64));
+    }
+
+    template<>
+    void EncodingUtility::PutEncodedValueInBuffer<uint16>(uint16 value, TArray<uint8>& buffer, int& offset)
+    {
+        PutToBuffer(buffer, (uint8*)&value, offset, sizeof(uint16));
+    }
+
+    template<>
+    void EncodingUtility::PutEncodedValueInBuffer<FVector3f>(FVector3f value, TArray<uint8>& buffer, int& offset)
+    {
+        PutEncodedValueInBuffer(value.X, buffer, offset);
+        PutEncodedValueInBuffer(value.Y, buffer, offset);
+        PutEncodedValueInBuffer(value.Z, buffer, offset);
+    }
+
+    template<>
+    void EncodingUtility::PutEncodedValueInBuffer<FIntVector>(FIntVector value, TArray<uint8>& buffer, int& offset)
+    {
+        PutEncodedValueInBuffer(value.X, buffer, offset);
+        PutEncodedValueInBuffer(value.Y, buffer, offset);
+        PutEncodedValueInBuffer(value.Z, buffer, offset);
+    }
+
+    template<>
+    void EncodingUtility::PutEncodedValueInBuffer<FVector2f>(FVector2f value, TArray<uint8>& buffer, int& offset)
+    {
+        PutEncodedValueInBuffer(value.X, buffer, offset);
+        PutEncodedValueInBuffer(value.Y, buffer, offset);
+    }
+
+    template<>
+    void EncodingUtility::PutEncodedValueInBuffer<FIntVector2>(FIntVector2 value, TArray<uint8>& buffer, int& offset)
+    {
+        PutEncodedValueInBuffer(value.X, buffer, offset);
+        PutEncodedValueInBuffer(value.Y, buffer, offset);
+    }
+
+    template<>
+    void EncodingUtility::PutEncodedValueInBuffer<FQuat4f>(FQuat4f value, TArray<uint8>& buffer, int& offset)
+    {
+        PutEncodedValueInBuffer(value.X, buffer, offset);
+        PutEncodedValueInBuffer(value.Y, buffer, offset);
+        PutEncodedValueInBuffer(value.Z, buffer, offset);
+        PutEncodedValueInBuffer(value.W, buffer, offset);
+    }
+
+#pragma endregion
+
+#pragma region Encode
     template <typename T> TArray<uint8> EncodingUtility::Encode(T value)
     {
         throw "The type " << T << "cannot be encoded yet";
     }
 
-#pragma region Encode
     template<> TArray<uint8> EncodingUtility::Encode<FString>(FString value)
     {
         return TArray<uint8>{ (const uint8*)TCHAR_TO_UTF8(*value), value.Len() };
@@ -67,14 +181,62 @@ namespace OnionMan::Network
         return TArray<uint8>{ (uint8*)&value, sizeof(uint16) };
     }
 
+    template<> TArray<uint8> EncodingUtility::Encode<FVector3f>(FVector3f value)
+    {
+        TArray<uint8> encodedQuat{};
+        encodedQuat.Reserve(3 * sizeof(float));
+        encodedQuat.Append(Encode(value.X));
+        encodedQuat.Append(Encode(value.Y));
+        encodedQuat.Append(Encode(value.Z));
+        return encodedQuat;
+    }
+
+    template<> TArray<uint8> EncodingUtility::Encode<FIntVector>(FIntVector value)
+    {
+        TArray<uint8> encodedQuat{};
+        encodedQuat.Reserve(3 * sizeof(int));
+        encodedQuat.Append(Encode(value.X));
+        encodedQuat.Append(Encode(value.Y));
+        encodedQuat.Append(Encode(value.Z));
+        return encodedQuat;
+    }
+
+    template<> TArray<uint8> EncodingUtility::Encode<FVector2f>(FVector2f value)
+    {
+        TArray<uint8> encodedQuat{};
+        encodedQuat.Reserve(2 * sizeof(float));
+        encodedQuat.Append(Encode(value.X));
+        encodedQuat.Append(Encode(value.Y));
+        return encodedQuat;
+    }
+
+    template<> TArray<uint8> EncodingUtility::Encode<FIntVector2>(FIntVector2 value)
+    {
+        TArray<uint8> encodedQuat{};
+        encodedQuat.Reserve(2 * sizeof(int));
+        encodedQuat.Append(Encode(value.X));
+        encodedQuat.Append(Encode(value.Y));
+        return encodedQuat;
+    }
+
+    template<> TArray<uint8> EncodingUtility::Encode<FQuat4f>(FQuat4f value)
+    {
+        TArray<uint8> encodedQuat{};
+        encodedQuat.Reserve(4 * sizeof(float));
+        encodedQuat.Append(Encode(value.X));
+        encodedQuat.Append(Encode(value.Y));
+        encodedQuat.Append(Encode(value.Z));
+        encodedQuat.Append(Encode(value.W));
+        return encodedQuat;
+    }
+
 #pragma endregion
 
+#pragma region Decode
     template <typename T> T EncodingUtility::Decode(TArray<uint8> bytes, int& offset, int size = -1)
     {
         throw "The type " << T << "cannot be decoded yet";
     }
-
-#pragma region Decode
 
     template<> FString EncodingUtility::Decode<FString>(TArray<uint8> bytes, int& offset, int size)
     {
@@ -157,6 +319,117 @@ namespace OnionMan::Network
         return decodedValue;
     }
 
+    template<> FVector3f EncodingUtility::Decode<FVector3f>(TArray<uint8> bytes, int& offset, int size)
+    {
+        return FVector3f(
+            Decode<float>(bytes, offset),
+            Decode<float>(bytes, offset),
+            Decode<float>(bytes, offset));
+    }
+
+    template<> FIntVector EncodingUtility::Decode<FIntVector>(TArray<uint8> bytes, int& offset, int size)
+    {
+        return FIntVector(
+            Decode<int>(bytes, offset),
+            Decode<int>(bytes, offset),
+            Decode<int>(bytes, offset));
+    }
+
+    template<> FVector2f EncodingUtility::Decode<FVector2f>(TArray<uint8> bytes, int& offset, int size)
+    {
+        return FVector2f(
+            Decode<float>(bytes, offset),
+            Decode<float>(bytes, offset));
+    }
+
+    template<> FIntVector2 EncodingUtility::Decode<FIntVector2>(TArray<uint8> bytes, int& offset, int size)
+    {
+        return FIntVector2(
+            Decode<int>(bytes, offset),
+            Decode<int>(bytes, offset));
+    }
+
+    template<> FQuat4f EncodingUtility::Decode<FQuat4f>(TArray<uint8> bytes, int& offset, int size)
+    {
+        return FQuat4f(
+            Decode<float>(bytes, offset),
+            Decode<float>(bytes, offset),
+            Decode<float>(bytes, offset),
+            Decode<float>(bytes, offset));
+    }
+
+#pragma endregion
+
+#pragma region PutToBuffer
+    void EncodingUtility::PutToBuffer(TArray<uint8>& toBuffer, TArray<uint8>& fromBuffer, int& offset)
+    {
+        for (int i{ 0 }; i < fromBuffer.Num(); i++, offset++)
+        {
+            toBuffer[offset] = fromBuffer[i];
+        }
+    }
+
+    void EncodingUtility::PutToBuffer(TArray<uint8>& toBuffer, const uint8* fromBuffer, int& offset, int fromBufferSize)
+    {
+        for (int i{ 0 }; i < fromBufferSize; i++, offset++)
+        {
+            toBuffer[offset] = fromBuffer[i];
+        }
+    }
+#pragma endregion
+
+#pragma region GetSizeOf
+
+    template<typename T>
+    int EncodingUtility::GetSizeOf(T value)
+    {
+        return sizeof(T);
+    }
+
+    template<>
+    int EncodingUtility::GetSizeOf<FString>(FString value)
+    {
+        return Encode(value).Num();
+    }
+
+    template<> int EncodingUtility::GetSizeOf<FVector3f>(FVector3f value)
+    {
+        return 3 * sizeof(float);
+    }
+
+    template<> int EncodingUtility::GetSizeOf<FIntVector>(FIntVector value)
+    {
+        return 3 * sizeof(int);
+    }
+
+    template<> int EncodingUtility::GetSizeOf<FVector2f>(FVector2f value)
+    {
+        return 2 * sizeof(float);
+    }
+
+    template<> int EncodingUtility::GetSizeOf<FIntVector2>(FIntVector2 value)
+    {
+        return 2 * sizeof(int);
+    }
+
+    template<> int EncodingUtility::GetSizeOf<FQuat4f>(FQuat4f value)
+    {
+        return 4 * sizeof(float);
+    }
+
+#pragma endregion
+
+#pragma region HasFixedEncodedSize
+    template<typename T>
+    bool EncodingUtility::HasFixedEncodedSize()
+    {
+        return true;
+    }
+    template<>
+    bool EncodingUtility::HasFixedEncodedSize<FString>()
+    {
+        return false;
+    }
 #pragma endregion
 
 #pragma region Debug
@@ -186,8 +459,4 @@ namespace OnionMan::Network
         return bytes;
     }
 #pragma endregion
-
 }
-
-
-
--- OnionMan_Unreal/Source/OnionMan_Unreal/Public/EncodingUtility.h
+++ OnionMan_Unreal/Source/OnionMan_Unreal/Public/EncodingUtility.h
@@ -17,11 +17,23 @@ namespace OnionMan:: Network
         ~EncodingUtility();
 
         template<typename T>
+        static void PutEncodedValueInBuffer(T value, TArray<uint8>& buffer, int& offset);
+
+        template<typename T>
         static TArray<uint8> Encode(T value);
 
         template<typename T>
         static T Decode(TArray<uint8> bytes, int& offset, int size = -1);
 
+        static void PutToBuffer(TArray<uint8>& toBuffer, TArray<uint8>& fromBuffer, int& offset);
+        static void PutToBuffer(TArray<uint8>& toBuffer, const uint8* fromBuffer, int& offset, int fromBufferSize);
+
+
+        template<typename T>
+        static int GetSizeOf(T value = default);
+
+        template<typename T>
+        static bool HasFixedEncodedSize();
 
         FString GetBytesAsString(TArray<uint8> bytes);
         TArray<uint8> GetStringAsBytes(FString stringifiedBytes);
