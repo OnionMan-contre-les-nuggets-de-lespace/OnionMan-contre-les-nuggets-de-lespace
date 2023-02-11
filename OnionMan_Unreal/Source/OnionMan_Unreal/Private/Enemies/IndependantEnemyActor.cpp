d1f976ca82da010abcb93f79338e9d2e22a1a9cb
paulluneaug <paul.luneau@mail.com>
2023-02-11 04:27:55 +0100

    Added an independent enemy that does not follow a spline

---
+++ OnionMan_Unreal/Source/OnionMan_Unreal/Private/Enemies/IndependantEnemyActor.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Enemies/IndependantEnemyActor.h"
+
+
+void AIndependantEnemyActor::Spawn(FVector3f startPosition)
+{
+}
+
+void AIndependantEnemyActor::Move(float deltaTime, float timeSinceSpawn)
+{
+}
+
+void AIndependantEnemyActor::EditorLoad(float timeSinceSpawn)
+{
+}
+
+void AIndependantEnemyActor::EditorUpdate(float newTimeSinceSpawn)
+{
+}
+
+void AIndependantEnemyActor::EditorUnload()
+{
+}
---
+++ OnionMan_Unreal/Source/OnionMan_Unreal/Public/Enemies/IndependantEnemyActor.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnemyActor.h"
+#include "IndependantEnemyActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ONIONMAN_UNREAL_API AIndependantEnemyActor : public AEnemyActor
+{
+	GENERATED_BODY()
+
+public:
+	void Spawn(FVector3f startPosition);
+
+	UFUNCTION(BlueprintCallable, Category = "Enemy")
+		virtual void Move(float deltaTime, float timeSinceSpawn) override;
+
+
+	// Editor
+	virtual void EditorLoad(float timeSinceSpawn) override;
+	virtual void EditorUpdate(float newTimeSinceSpawn) override;
+	virtual void EditorUnload() override;
+	
+};
