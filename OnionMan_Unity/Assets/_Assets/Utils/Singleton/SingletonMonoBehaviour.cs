70d74cca1997e2dffc3f8dfb07fc346e4649c0d7
pluneauAcreos <p.luneau@acreos.eu>
2023-03-27 17:16:10 +0200

    Fix style

--- OnionMan_Unity/Assets/_Assets/Utils/Singleton/SingletonMonoBehaviour.cs
+++ OnionMan_Unity/Assets/_Assets/Utils/Singleton/SingletonMonoBehaviour.cs
@@ -2,95 +2,95 @@ using UnityEngine;
 
 namespace OnionMan.Utils
 {
-	public abstract class SingletonMonoBehaviour<T> : MonoBehaviour, ISingleton<T> where T : SingletonMonoBehaviour<T>
-	{
-		protected static T s_instance;
+    public abstract class SingletonMonoBehaviour<T> : MonoBehaviour, ISingleton<T> where T : SingletonMonoBehaviour<T>
+    {
+        protected static T s_instance;
 
-		private static object m_lock = new object();
+        private static object m_lock = new object();
 
-		public static T Instance
-		{
-			get
-			{
-				if (m_applicationIsQuitting)
-				{
-					return null;
-				}
+        public static T Instance
+        {
+            get
+            {
+                if (m_applicationIsQuitting)
+                {
+                    return null;
+                }
 
-				lock (m_lock)
-				{
-					if (s_instance == null)
-					{
-						s_instance = (T)FindObjectOfType(typeof(T));
+                lock (m_lock)
+                {
+                    if (s_instance == null)
+                    {
+                        s_instance = (T)FindObjectOfType(typeof(T));
 
-						if (FindObjectsOfType(typeof(T)).Length > 1)
-						{
-							return s_instance;
-						}
+                        if (FindObjectsOfType(typeof(T)).Length > 1)
+                        {
+                            return s_instance;
+                        }
 
-						if (s_instance == null)
-						{
-							GameObject singleton = new GameObject();
-							s_instance = singleton.AddComponent<T>();
-							singleton.name = "[Singleton] " + typeof(T).ToString();
+                        if (s_instance == null)
+                        {
+                            GameObject singleton = new GameObject();
+                            s_instance = singleton.AddComponent<T>();
+                            singleton.name = "[Singleton] " + typeof(T).ToString();
 
-							DontDestroyOnLoad(singleton);
-						}
-					}
+                            DontDestroyOnLoad(singleton);
+                        }
+                    }
 
-					return s_instance;
-				}
-			}
-		}
+                    return s_instance;
+                }
+            }
+        }
 
-		private static bool m_applicationIsQuitting = false;
+        private static bool m_applicationIsQuitting = false;
 
-		public static bool ApplicationIsQuitting { set => m_applicationIsQuitting = value; }
+        public static bool ApplicationIsQuitting { set => m_applicationIsQuitting = value; }
 
-		protected virtual void Start()
-		{
-			if (s_instance != null && s_instance != this)
-			{
-				Destroy(gameObject);
-			}
-			else
-			{
-				DontDestroyOnLoad(gameObject);
-				s_instance = (T)FindObjectOfType(typeof(T));
+        protected virtual void Start()
+        {
+            if (s_instance != null && s_instance != this)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                DontDestroyOnLoad(gameObject);
+                s_instance = (T)FindObjectOfType(typeof(T));
 
-				Initialize();
-			}
-		}
+                Initialize();
+            }
+        }
 
-		/// <summary>
-		/// When Unity quits, it destroys objects in a random order.
-		/// In principle, a Singleton is only destroyed when application quits.
-		/// If any script calls Instance after it have been destroyed, 
-		///   it will create a buggy ghost object that will stay on the Editor scene
-		///   even after stopping playing the Application. Really bad!
-		/// So, this was made to be sure we're not creating that buggy ghost object.
-		/// </summary>
-		public virtual void OnDestroy()
-		{
-			if (s_instance != null && s_instance == this)
-			{
-				m_applicationIsQuitting = true;
-			}
-		}
+        /// <summary>
+        /// When Unity quits, it destroys objects in a random order.
+        /// In principle, a Singleton is only destroyed when application quits.
+        /// If any script calls Instance after it have been destroyed, 
+        ///   it will create a buggy ghost object that will stay on the Editor scene
+        ///   even after stopping playing the Application. Really bad!
+        /// So, this was made to be sure we're not creating that buggy ghost object.
+        /// </summary>
+        public virtual void OnDestroy()
+        {
+            if (s_instance != null && s_instance == this)
+            {
+                m_applicationIsQuitting = true;
+            }
+        }
 
-		public virtual void Initialize()
-		{
+        public virtual void Initialize()
+        {
 
-		}
+        }
 
-		public static bool IsInstanciated()
-		{
-			return s_instance != null;
-		}
+        public static bool IsInstanciated()
+        {
+            return s_instance != null;
+        }
 
-		public static void OverrideInstance(T instance)
-		{
-			s_instance = instance;
-		}
-	}
+        public static void OverrideInstance(T instance)
+        {
+            s_instance = instance;
+        }
+    }
 }
--- OnionMan_Unity/Assets/_Assets/Utils/Singleton/SingletonScriptable.cs
+++ OnionMan_Unity/Assets/_Assets/Utils/Singleton/SingletonScriptable.cs
@@ -6,26 +6,26 @@ using UnityEngine;
 
 namespace OnionMan.Utils
 {
-	public abstract class SingletonScriptable<T> : ScriptableObject, ISingleton<T> where T : SingletonScriptable<T>
-	{
-		private static T s_instance = default(T);
+    public abstract class SingletonScriptable<T> : ScriptableObject, ISingleton<T> where T : SingletonScriptable<T>
+    {
+        private static T s_instance = default(T);
 
-		public static T Instance
-		{
-			get
-			{
-				Load();
-				return s_instance;
-			}
-		}
+        public static T Instance
+        {
+            get
+            {
+                Load();
+                return s_instance;
+            }
+        }
 
-		public static void Load()
-		{
-			if (s_instance == null)
-			{
-				string classname = typeof(T).ToString();
-				s_instance = Resources.Load<T>(classname);
-			}
-		}
-	}
+        public static void Load()
+        {
+            if (s_instance == null)
+            {
+                string classname = typeof(T).ToString();
+                s_instance = Resources.Load<T>(classname);
+            }
+        }
+    }
 }
