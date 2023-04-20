35f9a8bcc0a0565c420308f525f2fadb1bbfc08b
paulluneaug <paul.luneau@mail.com>
2023-04-20 23:48:22 +0200

    UnityEntry Point

---
+++ OnionMan_Unity/Assets/Scripts/EntryPoint.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using OnionMan.Network;
+
+public class EntryPoint : MonoBehaviour
+{
+    [SerializeField] private NetworkManager m_networkManagerPrefab;
+    [SerializeField] private string m_mainMenuSceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NetworkManager networkManager = Instantiate(m_networkManagerPrefab);
+        networkManager.name = nameof(NetworkManager);
+        DontDestroyOnLoad(networkManager);
+        NetworkManager.Instance.Initialize();
+
+        Scene entryScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(m_mainMenuSceneName);
+        SceneManager.UnloadSceneAsync(entryScene);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
