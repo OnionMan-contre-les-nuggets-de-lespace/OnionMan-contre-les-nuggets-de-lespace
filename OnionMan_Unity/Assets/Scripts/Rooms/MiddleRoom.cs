e335fe8aa5658aa96091aaecc44580ee3d825344
Victor <victordesenneville@gmail.com>
2023-05-17 09:32:56 +0200

    Rooms decay

--- OnionMan_Unity/Assets/Scripts/Rooms/MiddleRoom.cs
+++ OnionMan_Unity/Assets/Scripts/Rooms/MiddleRoom.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using TMPro;
 
 public class MiddleRoom : BaseRoom
 {
     [SerializeField] private int roomHealth;
-    [SerializeField] private float timerBeforeDecay;
     [SerializeField] private int decayDamage;
     [SerializeField] private Vector2 rangeOfTimerBeforeDecay;
+    [SerializeField] private float m_timerBeforeStartDecay;
+    [SerializeField] private TMP_Text roomHealthText;
 
     private bool m_isInCriticalState;
+    private float m_timeBeforeDecay;
 
     private RoomAction_Scan scanAction;
     private RoomAction_Repair repairAction;
@@ -37,5 +40,38 @@ public class MiddleRoom : BaseRoom
         roomActions.Add(extinguishAction);
 
         return roomActions;
+    }
+
+    private void Update()
+    {
+        roomHealthText.text = roomHealth.ToString() + "%";
+
+        if(m_timerBeforeStartDecay > 0)
+        {
+            m_timerBeforeStartDecay -= Time.deltaTime;
+        }
+
+        if(m_timerBeforeStartDecay <= 0)
+        {
+            if (!m_isInCriticalState)
+            {
+                if (m_timeBeforeDecay <= 0)
+                {
+                    roomHealth -= decayDamage;
+                    m_timeBeforeDecay = UnityEngine.Random.Range(rangeOfTimerBeforeDecay.x, rangeOfTimerBeforeDecay.y);
+                }
+
+                if (m_timeBeforeDecay > 0)
+                {
+                    m_timeBeforeDecay -= Time.deltaTime;
+                }
+            }
+        }
+        
+    }
+
+    IEnumerator DecayCycleCoroutine()
+    {
+        yield return new WaitForSeconds(0);
     }
 }
