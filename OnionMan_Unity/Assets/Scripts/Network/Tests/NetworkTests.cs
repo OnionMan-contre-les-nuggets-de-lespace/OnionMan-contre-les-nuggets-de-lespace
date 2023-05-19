80506927770267e8f9c5e737beee1016302eadc0
paulluneaug <paul.luneau@mail.com>
2023-05-19 18:12:24 +0200

    Updated tests

--- OnionMan_Unity/Assets/Scripts/Network/Tests/NetworkTests.cs
+++ OnionMan_Unity/Assets/Scripts/Network/Tests/NetworkTests.cs
@@ -33,6 +33,41 @@ public class NetworkTests : SynchronizedMonoBehaviour
         //NetworkManager.Instance.DecodeObjects(NetworkManager.Instance.EncodeObjects());
     }
 
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R)) { RandomizeValues(); }
+    }
+
+    public void RandomizeValues()
+    {
+        int SP0NewCount = Random.Range(2, 10);
+        SP0.Value.Clear();
+        for (int i = 0; i < SP0NewCount; i++)
+        {
+            SP0.Value.Add(GetRandomString());
+        }
+
+        SP2.Value = new Quaternion(Random.value, Random.value, Random.value, Random.value);
+
+        SP3.Value = Random.value > 0.5f;
+
+        Debug.LogError($"RandomizedValues");
+    }
+
+    private string GetRandomString()
+    {
+        int stringLen = Random.Range(5, 20);
+        byte[] encodedString = new byte[stringLen * 2];
+        for (int i = 0; i < stringLen * 2; i++) 
+        {
+            encodedString[i] = (byte)Random.Range(0, 256);
+        }
+        int offset = 0;
+        string randomString = EncodingUtility.Decode<string>(encodedString, ref offset, stringLen);
+        Debug.LogError(randomString);
+        return randomString;
+    }
+
     public void TryDecode()
     {
         NetworkManager.Instance.DecodeObjects(EncodingUtility.GetStringAsBytes(m_encodedObjects));
