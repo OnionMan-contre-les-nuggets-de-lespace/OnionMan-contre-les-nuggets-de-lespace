7bbc1e267f1928ce6e4bc6d71c778de0bb40bceb
Victor <victordesenneville@gmail.com>
2023-06-18 17:39:11 +0200

    Added getter for Synchronized Property

--- OnionMan_Unity/Assets/Scripts/SynchronizedObjects/SO_PlayerHit.cs
+++ OnionMan_Unity/Assets/Scripts/SynchronizedObjects/SO_PlayerHit.cs
@@ -7,6 +7,16 @@ public class SO_PlayerHit : SynchronizedMonoBehaviour
 {
     [SerializeField] private SynchronizedProperty<bool> m_isPlayerHit;
     [SerializeField] private SynchronizedProperty<bool> m_hasReperaidCriticalRoom;
+
+    public SynchronizedProperty<bool> SP_IsPlayerHit
+    {
+        get => m_isPlayerHit;
+    }
+    public SynchronizedProperty<bool> HasReperaidCriticalRoom
+    {
+        get => m_hasReperaidCriticalRoom;
+    }
+
     public override void LoadProperties()
     {
         AddSynchronizedProperty(m_hasReperaidCriticalRoom);
--- OnionMan_Unity/Assets/Scripts/SynchronizedObjects/SO_ShipParticles.cs
+++ OnionMan_Unity/Assets/Scripts/SynchronizedObjects/SO_ShipParticles.cs
@@ -7,6 +7,17 @@ public class SO_ShipParticles : SynchronizedMonoBehaviour
 {
     [SerializeField] private SynchronizedProperty<bool> m_roomIsScanned;
     [SerializeField] private SynchronizedProperty<Vector3Int> m_rightComputerResultAnswer;
+
+    public SynchronizedProperty<bool> SP_RoomIsScanned
+    {
+        get => m_roomIsScanned;
+    }
+    public Vector3Int SP_RightComputerResultAnswer
+    {
+        get => m_rightComputerResultAnswer.Value;
+    }
+
+
     public override void LoadProperties()
     {
         AddSynchronizedProperty(m_roomIsScanned);
